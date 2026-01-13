6fe2da94c0d114374891c1dcbfb3fc2a06941837
songhao2103 <braumsp1@gmail.com>
2026-01-13 22:19:45 +0700

    fix: duplicate footer

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,13 +1,9 @@
-import { Footer } from '@/components/layout/Footer'
-import { Header } from '@/components/layout/Header'
 import { ContactSection } from '@/components/sections/ContactSection'
 
 export default function ContactPage() {
   return (
     <main className="min-h-screen">
-      <Header />
       <ContactSection />
-      <Footer />
     </main>
   )
 }
